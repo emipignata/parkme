0e70ed4728fede968dc1b8b2497e5fe0ea08a891
emipignata <emipignata@gmail.com>
2023-09-30 23:49:53 -0300

    Agrego el RecyclerView a la pantalla Explore para poder navegar las cocheras. la lista quedo harcodeada hasta que el team back termine el endpoint para consumir de la bbdd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.3")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
