7f1efa86e8884652786c5bacb615e5e4132c4764
emipignata <emipignata@gmail.com>
2023-10-13 16:50:44 -0300

    subo la pantalla mis cocheras, quiza le podemos hacer que el scroll sea swipe

--- app/src/main/java/com/example/parkme/navigation/MisCocherasFr.kt
+++ app/src/main/java/com/example/parkme/navigation/MisCocherasFr.kt
@@ -1,19 +1,48 @@
 package com.example.parkme.navigation
 
+
+import com.example.parkme.R
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.parkme.R
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.parkme.adapter.MisCocherasAdapter
+import com.example.parkme.models.ItemMisCocheras
 
 class MisCocherasFr : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mis_cocheras, container, false)
+        val view = inflater.inflate(R.layout.fragment_mis_cocheras, container, false)
+
+        recyclerView = view.findViewById(R.id.recyclerViewMisCocheras) // Asegúrate de que el RecyclerView esté definido en fragment_mis_cocheras.xml
+
+        // Crea una lista de datos de ejemplo (puedes reemplazar esto con tus propios datos)
+        val cocherasList = listOf(
+        ItemMisCocheras("Título 1", "Descripción 1", R.drawable.cochera),
+        ItemMisCocheras("Título 2", "Descripción 2", R.drawable.cochera),
+        ItemMisCocheras("Título 3", "Descripción 3", R.drawable.cochera),
+        ItemMisCocheras("Título 4", "Descripción 4", R.drawable.cochera),
+        ItemMisCocheras("Título 5", "Descripción 5", R.drawable.cochera),
+        ItemMisCocheras("Título 6", "Descripción 6", R.drawable.cochera),
+        ItemMisCocheras("Título 7", "Descripción 7", R.drawable.cochera),
+        ItemMisCocheras("Título 8", "Descripción 8", R.drawable.cochera),
+        ItemMisCocheras("Título 9", "Descripción 9", R.drawable.cochera),
+        ItemMisCocheras("Título 10", "Descripción 10", R.drawable.cochera)
+        )
+
+        // Configura un adaptador personalizado para el RecyclerView
+        val adapter = MisCocherasAdapter(cocherasList)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        return view
     }
 }
