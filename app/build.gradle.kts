6e7f50fb61ecb51b7befb0e43b3efc37a5104416
Nicolas Wainschenker <nwainschenker@prismamp.com>
2023-10-15 04:42:53 -0300

    merge from main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -46,6 +47,7 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.firebase:firebase-auth:22.1.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/parkme/navigation/PerfilFr.kt
+++ app/src/main/java/com/example/parkme/navigation/PerfilFr.kt
@@ -1,20 +1,61 @@
 package com.example.parkme.navigation
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.example.parkme.R
+import com.example.parkme.SplashScreenActivity
+import com.example.parkme.databinding.FragmentPerfilBinding
+import com.bumptech.glide.Glide
 
 class PerfilFr : Fragment() {
+    private lateinit var binding: FragmentPerfilBinding
+    private val mGoogleSignInClient: GoogleSignInClient by lazy {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.web_client_id))
+            .requestEmail()
+            .build()
+        GoogleSignIn.getClient(requireContext(), gso)
+    }
+    private val firebaseAuth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+    private val currentUser: FirebaseUser by lazy { firebaseAuth.currentUser!! }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false)
-    }
+    ): View {
+        binding = FragmentPerfilBinding.inflate(inflater, container, false)
+        val view = binding.root
+        val btnLogout = view.findViewById<Button>(R.id.btnLogout)
 
+        binding.profileEmail.text = currentUser.email
+        binding.profileName.text = currentUser.displayName
+        binding.profileLastname.text = currentUser.displayName
+        Glide
+            .with(binding.root.context)
+            .load(currentUser.photoUrl)
+            .into(binding.profileImage)
+        btnLogout.setOnClickListener {
+            mGoogleSignInClient.signOut().addOnCompleteListener {
+                firebaseAuth.signOut()
+                Log.e("USUARIO ACTUAL EN PERFIL:", "Estado Usuario: ${firebaseAuth.currentUser}")
+                val intent = Intent(requireContext(), SplashScreenActivity::class.java)
+                Toast.makeText(requireContext(), mGoogleSignInClient.toString(), Toast.LENGTH_SHORT).show()
+                startActivity(intent)
+            }
+        }
+        return view
+    }
 }
