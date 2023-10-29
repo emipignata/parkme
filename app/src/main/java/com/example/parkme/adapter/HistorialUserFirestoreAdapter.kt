5681b0fae1a1b7858139ad427fa090a935094d1d
Federico Facchin <federico.nicolas.facchin@gmail.com>
2023-10-29 17:45:46 -0300

    se agrego gpay

--- app/src/main/java/com/example/parkme/adapter/HistorialUserFirestoreAdapter.kt
+++ app/src/main/java/com/example/parkme/adapter/HistorialUserFirestoreAdapter.kt
@@ -2,19 +2,28 @@ package com.example.parkme.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.parkme.R
 import com.example.parkme.entities.Reserva
 import com.example.parkme.holders.ReservaHolder
+import com.example.parkme.navigation.HistorialFrDirections
+import com.example.parkme.navigation.MisCocherasFrDirections
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 class HistorialUserFirestoreAdapter(options: FirestoreRecyclerOptions<Reserva>) :
     FirestoreRecyclerAdapter<Reserva, ReservaHolder>(options) {
     override fun onBindViewHolder(holder: ReservaHolder, position: Int, model: Reserva) {
         holder.setCard(model.reservaId, model.usuarioId, model.fecha, model.cocheraId, model.ownerId, model.precio)
+        holder.itemView.setOnClickListener {
+            val action = HistorialFrDirections.actionHistorialFrToCheckoutActivity(100.0f)
+            holder.itemView.findNavController().navigate(action)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReservaHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.historial_item, parent, false)
         return ReservaHolder(view)
     }
+
+
 }
